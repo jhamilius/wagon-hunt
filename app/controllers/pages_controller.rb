01f4f60af454a3a5b74dff3a23ab646b8b04bfdc
Julien <julien.ha@gmail.com>
2016-06-05 19:22:58 +0200

    .

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+  	@today = Date.today
   end
+  def team
+  	@members = ["Bob","romain","Seb"]
+
+  end
+  def join_us
+  	render plain: "Join Us"
+  end
+
 end
